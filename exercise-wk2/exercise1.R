25a3389fa5046ca10f4e04541a04a4c6d2211c2d
Beichen Liang <liangbeichen@hotmail.com>
2018-01-17 17:09:11 -0800

    complete part 1 and part of part 2

--- exercise-wk2/exercise1.R
+++ exercise-wk2/exercise1.R
@@ -1,38 +1,45 @@
 ## Part 1: Debugging
 
 my.num <- 6
-initials <- "?. ?."
+initials <- "B.c .L"
 
 my.vector <- c(my.num, initials)
-
 # run ?sum to get more info
-vector.sum <- sum(my.vector)
+# vector.sum <- sum(my.vector)
 
 # Describe why this doesn't work: 
-# 
+# different types 
 
-install.packages("stringr")
+# install.packages("stringr")
 
 my.line <- "Hey, hey, this is the library"
 
 print(str_length(my.line))
 
 # Describe why this doesn't work: 
-# 
+# not loaded the library yet
 
-said.the.famous <- paste(my.line, " - ", initial)
+said.the.famous <- paste(my.line, " - ", initials)
 
 # Describe why this doesn't work: 
-# 
+# variable name typo
 
 
 ## Part 2 - Vector and function practice
 
 # Make a vector and use typeof to check what type R considers it to be
-
+my.six <- c(6,  6)
+typeof(my.six)
+typeof(my.vector)
 # Write a function `CompareLength` that takes in 2 vectors, and returns the sentence:
 # "The difference in lengths is N"
-
+CompareLength <- function(c1, c2) {
+  v1 <- length(c1)
+  v2 <- length(c2)
+  dif <- abs(v1-v2)
+  phrase <- paste("The difference in lengths is", dif)
+  return(phrase)
+}
 
 # Pass two vectors of different length to your `CompareLength` function
 
